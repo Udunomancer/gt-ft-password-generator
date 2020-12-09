2f5ce70759c0c920110a7df5e50bb4d9eb808cea
Udunomancer <kyle717@gmail.com>
2020-12-09 09:59:07 -0500

    added working return to generatePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -9,13 +9,33 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 var intDict = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var alphaDict = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var specialDict = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+'];
 
 function generatePassword() {
-  return 'Password';
+
+  var userInputLen = prompt('How long would you like your password to be?\n(Response must be in numeric characters and passowrd length must be between 8 and 128 characters long)');
+  return userInputLen;
 }
+
+// Click button to start
+
+// Prompted for password criteria
+  // 1. Prompt for password length
+    // Verify that value entered is a numeric value
+    // Verify that numberic value entered is between 8 and 128
+  // 2. Prompt for lowercase characters
+  // 3. Prompt to include uppercase characters
+  // 4. Prompt to include numerals
+  // 5. Prompt to include special characters
+    // Verify that at least one character type was selected
+
+// Function to return a randomly generated password bassed on user prompts
+  // Function to determine ratio of selected character types
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

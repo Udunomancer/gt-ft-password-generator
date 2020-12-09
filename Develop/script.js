df1800b24aae93255c95cf4f42dedd30cdf64b62
Udunomancer <kyle717@gmail.com>
2020-12-09 09:50:07 -0500

    added homework files and character dictionaries to script.js

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+var intDict = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var alphaDict = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialDict = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+'];
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
